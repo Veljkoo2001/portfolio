4aa180be6e2a1594f62a14264ab42fa3baad4892
Veljkoo2001 <veljkojakovljevic56@gmail.com>
2026-02-11 17:36:47 +0100

    Final version of the portfolio

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -102,6 +102,10 @@ DATABASES = {
     }
 }
 
+DEBUG = False
+
+ALLOWED_HOSTS = ['Veljko2001.pythonanywhere.com']
+
 
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
