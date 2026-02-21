e884f775a720f158d5e8dcc1df123a59c563f3ca
Veljkoo2001 <veljkojakovljevic56@gmail.com>
2026-02-21 12:14:07 +0100

    Removed unused files and updated config

--- portfolio_project/settings.py
+++ portfolio_project/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-dev-key-only')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', 'True') == 'True'
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
@@ -159,7 +159,7 @@ else:
     EMAIL_HOST = 'smtp.gmail.com'
     EMAIL_PORT = 587
     EMAIL_USE_TLS = True
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')         # GitHub Secret
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER', 'veljkojakovljevic1@gmail.com')         # GitHub Secret
     EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD') # GitHub Secret
     DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
